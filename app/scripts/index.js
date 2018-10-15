6c05a16e9114f092dac6ab516cf5b3eab2c4b5fd
Jordan S <jpstanway@gmail.com>
2018-10-15 14:42:30 -0700

    create tooltips, color code data points, and add a legend for reference

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,6 @@
 
 // declare variables for api
-let req, json, year, time, newTime;
+let req, json, year, time, newTime, color;
 const dataset = [];
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
@@ -20,10 +20,16 @@ req.onload = function() {
         newTime.setMinutes(time[0]);
         newTime.setSeconds(time[1]);
 
+        color = !cyclist.Doping ? '#808b3d' : '#483d8b';
+
         dataset.push({
             time: newTime,
             year: year,
-            name: cyclist.Name
+            name: cyclist.Name,
+            desc: cyclist.Doping || 'No record of doping',
+            country: cyclist.Nationality,
+            link: cyclist.URL,
+            color: color
         });
     });
 
@@ -32,11 +38,42 @@ req.onload = function() {
     const height = 600;
     const padding = 35;
 
+    // functions for formatting dates
+    function formatDate(type) {
+        if(type === 'Y') {
+            return d3.timeFormat('%Y');
+        } else if (type === 'M') {
+            return d3.timeFormat('%M:%S');
+        } else {
+            return false;
+        }
+    }
+
+    // create tooltip
+    const tip = d3.tip()
+                .attr('class', 'd3-tip')
+                .attr('id', 'tooltip')
+                .html((d) => {
+                    const year = formatDate('Y');
+                    const time = formatDate('M');
+
+                    d3.select('#tooltip').attr('data-year', year(d.year));
+
+                    return `
+                        ${d.name} (${d.country})<br>
+                        Time: ${time(d.time)} Year: ${year(d.year)}<br>
+                        <br>
+                        ${d.desc}
+                    `;
+                });                 
+
     // create svg area
     const svg = d3.select('#container')
                     .append('svg')
                     .attr('width', width)
-                    .attr('height', height);
+                    .attr('height', height)
+                    .call(tip);
+           
 
     // declare scales
     const xScale = d3.scaleLinear()
@@ -56,14 +93,65 @@ req.onload = function() {
         .attr('cy', (d) => yScale(d.time))
         .attr('r', (d) => 5)
         .attr('class', 'dot')
-        .attr('data-xvalue', (d) => d.year)
-        .attr('data-yvalue', (d) => d.time);
+        .attr('data-xvalue', (d) => {
+            const format = formatDate('Y');
+            return format(d.year);
+        })
+        .attr('data-yvalue', (d) => d.time)
+        .style('fill', (d) => d.color)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
+    // create and append a legend
+    svg.append('rect')
+        .attr('id', 'legend')
+        .attr('width', 250)
+        .attr('height', 100)
+        .attr('x', width - padding - 250)
+        .attr('y', 0)
+        .style('fill', '#efefef');
+
+    svg.append('text')
+        .attr('id', 'legend-title')
+        .attr('x', width - padding - 150)
+        .attr('y', 15)
+        .text('Record');    
+
+    // create symbols and append to legend
+    svg.append('rect')
+        .attr('class', 'symbol')
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('x', width - padding - 87.5)
+        .attr('y', 60)
+        .style('fill', '#483d8b');
+
+    svg.append('rect')
+        .attr('class', 'symbol')
+        .attr('width', 25)
+        .attr('height', 25)
+        .attr('x', width - padding - 187.5)
+        .attr('y', 60)
+        .style('fill', '#808b3d');
+
+    svg.append('text')
+        .attr('class', 'symbol-text')
+        .attr('x', width - padding - 190)
+        .attr('y', 50)
+        .text('clean');    
+
+    svg.append('text')
+        .attr('class', 'symbol-text')
+        .attr('x', width - padding - 90)
+        .attr('y', 50)
+        .text('dirty');    
+
 
     // create chart axes
     const xAxis = d3.axisBottom(xScale)
-                    .tickFormat(d3.timeFormat('%Y'));
+                    .tickFormat(formatDate('Y'));
     const yAxis = d3.axisLeft(yScale)
-                    .tickFormat(d3.timeFormat('%M:%S'));
+                    .tickFormat(formatDate('M'));
 
     // append axes to svg
     svg.append('g')
